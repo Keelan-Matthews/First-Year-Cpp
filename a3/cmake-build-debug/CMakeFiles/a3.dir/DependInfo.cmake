0ddaebd87174783f9cbf82af548779a55c96e29e
Keelan Matthews <90719883+Keelan-Matthews@users.noreply.github.com>
2021-12-27 11:25:28 +0200

    Add files via upload

---
+++ a3/cmake-build-debug/CMakeFiles/a3.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/Babushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/Babushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/BlueBabushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/BlueBabushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/Controller.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/Controller.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/ControllerException.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/ControllerException.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/DarkBlueBabushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/DarkBlueBabushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/DisplacementException.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/DisplacementException.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/GoldBabushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/GoldBabushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/GreenBabushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/GreenBabushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/LightBlueBabushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/LightBlueBabushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/OverflowException.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/OverflowException.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/RedBabushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/RedBabushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/RotateException.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/RotateException.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/UnderflowException.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/UnderflowException.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/YellowBabushka.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/YellowBabushka.cpp.obj"
+  "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/main.cpp" "G:/My Drive/University/Semester 2/COS 110/Coding/assignments/a3/cmake-build-debug/CMakeFiles/a3.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ a3/cmake-build-debug/CMakeFiles/a3.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a3.dir/Babushka.cpp.obj"
+  "CMakeFiles/a3.dir/BlueBabushka.cpp.obj"
+  "CMakeFiles/a3.dir/Controller.cpp.obj"
+  "CMakeFiles/a3.dir/ControllerException.cpp.obj"
+  "CMakeFiles/a3.dir/DarkBlueBabushka.cpp.obj"
+  "CMakeFiles/a3.dir/DisplacementException.cpp.obj"
+  "CMakeFiles/a3.dir/GoldBabushka.cpp.obj"
+  "CMakeFiles/a3.dir/GreenBabushka.cpp.obj"
+  "CMakeFiles/a3.dir/LightBlueBabushka.cpp.obj"
+  "CMakeFiles/a3.dir/OverflowException.cpp.obj"
+  "CMakeFiles/a3.dir/RedBabushka.cpp.obj"
+  "CMakeFiles/a3.dir/RotateException.cpp.obj"
+  "CMakeFiles/a3.dir/UnderflowException.cpp.obj"
+  "CMakeFiles/a3.dir/YellowBabushka.cpp.obj"
+  "CMakeFiles/a3.dir/main.cpp.obj"
+  "a3.exe"
+  "a3.exe.manifest"
+  "a3.pdb"
+  "liba3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
